6618d69006f13fd96fa4ea697c39c4d70ca3cf6f
irazakharchenko <zakharchenko@ucu.edu.ua>
2017-11-05 10:48:05 +0200

    make better balance

--- src/main/java/additional_assigm/SwordsMan.java
+++ src/main/java/additional_assigm/SwordsMan.java
@@ -8,8 +8,9 @@ public class SwordsMan extends Character{
     protected int MAX_POWER;
     protected int MIN_POWER;
     public SwordsMan(int power_min, int power_max, int hp_min, int hp_max, String name){
-
-        super(power_min, new Random().nextInt(hp_max - hp_min) + hp_min, name);
+        // we take random power as elf's kick depends on power his enemy
+        // but when swordsman kick it will be another power
+        super(new Random().nextInt(power_max - power_min) + power_min, new Random().nextInt(hp_max - hp_min) + hp_min, name);
         MAX_POWER = power_max;
         MIN_POWER = power_min;
         this.name = name;
